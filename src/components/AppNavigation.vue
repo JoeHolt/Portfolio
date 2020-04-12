5f1ac12ec1baa2dd3c433262b3622035ab540e4e
JoeHolt <jpholt2@wisc.edu>
2020-04-12 17:00:25 -0500

    added title bar

---
+++ src/components/AppNavigation.vue
@@ -0,0 +1,48 @@
+<!-- https://www.freecodecamp.org/news/how-to-build-a-single-page-application-using-vue-js-vuex-vuetify-and-firebase-838b40721a07/ -->
+<template>
+  <span>
+    <!-- Only visible on smaller displays -->
+    <v-navigation-drawer
+      app
+      v-model="drawer"
+      color="#b2bec3"
+      disable-resize-watcher
+    >
+      <v-list>
+        <template v-for="(item, index) in items">
+          <v-list-tile :key="index">
+            <v-list-tile-content>
+              {{ item.title }}
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-divider :key="`divider-${index}`"></v-divider>
+        </template>
+      </v-list>
+    </v-navigation-drawer>
+    <!-- View on larger displays -->
+    <v-toolbar app>
+      <v-toolbar-side-icon color="black" class="hidden-md-and-up" @click="drawer = !drawer">
+        <v-icon>mdi-dots-vertical</v-icon>  
+      </v-toolbar-side-icon>
+      <v-spacer class="hidden-md-and-up"></v-spacer>
+      <v-toolbar-title>{{ appTitle }}</v-toolbar-title>
+      <v-spacer class="hidden-sm-and-down"></v-spacer>
+      <div v-for="(item, index) in items" :key="index">
+        <v-btn flat class="hidden-sm-and-down">{{ item.title }}</v-btn>
+      </div>
+    </v-toolbar>
+  </span>
+</template>
+<script>
+// @ is an alias to /src
+export default {
+  name: 'AppNavigation',
+  data() {
+    return {
+      appTitle: 'JOE HOLT',
+      drawer: false,
+      items: [{ title: 'Menu' }, { title: 'Sign In' }, { title: 'Join' }]
+    };
+  }
+};
+</script>
