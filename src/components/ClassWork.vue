3ff2ce074f99d0638af3be3997610d77ec0d44e2
JoeHolt <jpholt2@wisc.edu>
2020-12-17 11:48:21 -0600

    JHD--0004: Refactored Course Viewer into own Component

---
+++ src/components/ClassWork.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-dialog v-model="open" width="35vw">
+    <template v-slot:activator="{ on }">
+      <v-btn
+        text
+        class="font-weight-black mb-1"
+        color="#6c5ce7"
+        v-on="on"
+      >
+        {{ skill.title }}
+      </v-btn>
+    </template>
+    <!-- pop over view -->
+    <v-toolbar
+      color="#6c5ce7"
+      class="courses--header headline font-weight-bold"
+      dark
+      flat
+    >
+      <v-toolbar-title class="noselect"
+        >{{ company }} -
+        {{ skill.title }}</v-toolbar-title
+      >
+      <v-spacer></v-spacer>
+      <v-btn icon @click="coursesOpened = false">
+        <v-icon>mdi-close</v-icon>
+      </v-btn>
+    </v-toolbar>
+    <!-- Listing skills -->
+    <v-list two-line subheader class="noselect">
+      <div v-for="subject in skill.expanded" :key="subject.title">
+        <v-subheader>{{ subject.category }}</v-subheader>
+        <v-list-item
+          v-for="course in subject.items"
+          :key="course.name"
+        >
+          <v-list-item-content>
+            <v-list-item-title>{{ course.name }}</v-list-item-title>
+            <v-list-item-subtitle
+              class="course--subtitle"
+              v-text="course.description"
+            ></v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </div>
+    </v-list>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  name: 'ClassWork',
+  props: {
+    company: String,
+    skill: Object,
+  },
+  data() {
+    return {
+      open: false
+    };
+  },
+  methods: {
+  }
+};
+</script>
+
+<style scoped></style>
--- src/components/ExperienceTimeline.vue
+++ src/components/ExperienceTimeline.vue
@@ -49,52 +49,11 @@
                 :category="skill.category"
               ></skill-pill>
               <div v-else style="width: 100%;">
-                <v-dialog v-model="coursesOpened" width="35vw">
-                  <template v-slot:activator="{ on }">
-                    <v-btn
-                      text
-                      class="font-weight-black mb-1"
-                      color="#6c5ce7"
-                      v-on="on"
-                    >
-                      {{ skill.title }}
-                    </v-btn>
-                  </template>
-                  <!-- pop over view -->
-                  <v-toolbar
-                    color="#6c5ce7"
-                    class="courses--header headline font-weight-bold"
-                    dark
-                    flat
+                <class-work
+                  :company="experience.company"
+                  :skill="skill"
                   >
-                    <v-toolbar-title class="noselect"
-                      >{{ experience.company }} -
-                      {{ skill.title }}</v-toolbar-title
-                    >
-                    <v-spacer></v-spacer>
-                    <v-btn icon @click="coursesOpened = false">
-                      <v-icon>mdi-close</v-icon>
-                    </v-btn>
-                  </v-toolbar>
-                  <!-- Listing skills -->
-                  <v-list two-line subheader class="noselect">
-                    <div v-for="subject in skill.expanded" :key="subject.title">
-                      <v-subheader>{{ subject.category }}</v-subheader>
-                      <v-list-item
-                        v-for="course in subject.items"
-                        :key="course.name"
-                      >
-                        <v-list-item-content>
-                          <v-list-item-title>{{ course.name }}</v-list-item-title>
-                          <v-list-item-subtitle
-                            class="course--subtitle"
-                            v-text="course.description"
-                          ></v-list-item-subtitle>
-                        </v-list-item-content>
-                      </v-list-item>
-                    </div>
-                  </v-list>
-                </v-dialog>
+                </class-work>
               </div>
             </div>
           </v-card-actions>
@@ -106,11 +65,13 @@
 
 <script>
 import SkillPill from '@/components/SkillPill';
+import ClassWork from '@/components/ClassWork';
 import experience from '@/data/experience';
 export default {
   name: 'ExperienceTimeline',
   components: {
-    SkillPill
+    SkillPill,
+    ClassWork
   },
   data () {
     return {
