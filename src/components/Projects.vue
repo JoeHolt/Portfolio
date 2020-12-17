da48b720ac1bb23bbb1d3d0e3f043d33867c6483
JoeHolt <jpholt2@wisc.edu>
2020-12-17 14:00:57 -0600

    JHD-0008: Added Hand Gesture Recognition Project
    - Also refactored project data into its own file

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -93,6 +93,7 @@
 
 <script>
 import SkillPill from '@/components/SkillPill';
+import projectData from '@/data/projects';
 export default {
   name: 'Projects',
   components: {
@@ -100,129 +101,7 @@ export default {
   },
   data() {
     return {
-      projects: [
-        {
-          name: 'WiscEnroll Mobile',
-          color1: '#9b0000',
-          color2: '#9b0000',
-          show: false,
-          img_small: false,
-          img: 'mdi-school-outline',
-          no_url: true,
-          url: 'https://www.google.com',
-          subtitle: 'Course Enroll for iOS Devices',
-          description:
-            "I created an iOS front end for UW-Madison's course enrollment app, pulling and updating live user data from the UW system. The source code is private.",
-          skills: [
-            { title: 'Swift', category: 'lang' },
-            { title: 'SwiftUI', category: 'tool' },
-            { title: 'MVC', category: 'dev' },
-            { title: 'URLRequest', category: 'tool' },
-            { title: 'iOS', category: 'platform' },
-            { title: 'macOS', category: 'platform' }
-          ]
-        },
-        {
-          name: 'SwiftBoard for iPhone',
-          color1: '#6c5ce7',
-          color2: '#6c5ce7',
-          show: false,
-          img_small: false,
-          img: 'mdi-keyboard',
-          no_url: true,
-          url: 'https://www.google.com',
-          subtitle: 'An Android-esque keyboard for iOS',
-          description:
-            'While in high school, I developed and release SwiftBoard for iPhone as my first app on the App Store. SwiftBoard is an iOS keyboard extension that adds extra functionality on top of the stock keyboard. The source code is private.',
-          skills: [
-            { title: 'Swift', category: 'lang' },
-            { title: 'UIKit', category: 'tool' },
-            { title: '10,000+ units', category: 'acomplisment' },
-            { title: 'Xcode', category: 'tool' },
-            { title: '$5,000+ revenue', category: 'acomplisment' },
-            { title: 'MVC', category: 'dev' },
-            { title: 'iOS', category: 'platform' }
-          ]
-        },
-        {
-          name: 'NEXT Face Demo',
-          color1: '#9b0000',
-          color2: '#9b0000',
-          show: false,
-          img_small: true,
-          img: 'mdi-face-recognition',
-          no_url: false,
-          url: 'https://github.com/stsievert/next-face-demo',
-          subtitle: 'Predict Facial Emotion',
-          description:
-            "Alongside UW-Madison NEXT researchers, I developed a demo that, given an input of the user's face, predicts their emotion. My demo was showcased at the Wisconsin Science Festival.",
-          skills: [
-            { title: 'Python', category: 'lang' },
-            { title: 'Bokeh', category: 'tool' },
-            { title: 'Jupyter', category: 'dev' },
-            { title: 'SciKitLearn', category: 'tool' },
-            { title: 'Web', category: 'platform' },
-            { title: 'AI', category: 'tool' }
-          ]
-        },
-        {
-          name: 'JoeHolt.dev',
-          color1: '#00c853',
-          color2: '#00c853',
-          show: false,
-          img_small: false,
-          img: 'mdi-chart-bubble',
-          url: 'https://github.com/JoeHolt/joeholt.github.io',
-          subtitle: 'The Best Website Out There',
-          description: "I created the website you are currently reading as a way to showcase the work I have done in the field of computer science. The website represents the cumulation of my design skills.",
-          skills: [
-            { title: 'JavaScript', category: 'lang' },
-            { title: 'Vue', category: 'tool' },
-            { title: 'Design', category: 'dev' },
-            { title: 'MVC', category: 'dev' },
-            { title: 'Web', category: 'platform' }
-          ]
-        },
-        {
-          name: 'Spartan',
-          color1: '#3d5afe',
-          color2: '#3d5afe',
-          show: false,
-          img_small: true,
-          img: 'mdi-align-vertical-bottom',
-          url: 'https://github.com/JoeHolt/Spartan-Manufacturing-Web',
-          subtitle: 'Inventory Tracker for my High School',
-          description:
-            'While in high school, I created a multiplatform inventory tracker for my school\'s tech ed department. The tool was designed to track inventories of raw materials, and completed products while being supported on a variety of platforms. The tool is still used today.',
-          skills: [
-            { title: 'Python', category: 'lang' },
-            { title: 'JavaScript', category: 'lang' },
-            { title: 'Swift', category: 'lang' },
-            { title: 'Multiplatform', category: 'platform' },
-            { title: 'Web', category: 'platform' },
-            { title: 'iOS', category: 'platform' },
-            { title: 'macOS', category: 'platform' }
-          ]
-        },
-        {
-          name: 'AHA: AI Assisted Humor',
-          color1: '#ff6f00',
-          color2: '#ff6f00',
-          show: false,
-          img_small: true,
-          img: 'mdi-emoticon-lol-outline',
-          url: 'https://github.com/nextml/AHA',
-          subtitle: 'Preduct Caption Relative Humor',
-          description:
-            'Working along side my fellow researchers, I created a demo that assists users by predicting the relative humor in captions for a New Yorker Caption Contest cartoon.',
-          skills: [
-            { title: 'Python', category: 'lang' },
-            { title: 'JavaScript', category: 'lang' },
-            { title: 'Web', category: 'platform' },
-            { title: 'AI', category: 'tool' }
-          ]
-        }
-      ]
+      projects: []
     };
   },
   methods: {
@@ -236,7 +115,9 @@ export default {
       return sty;
     }
   },
-  created() {}
+  created() {
+    this.projects = projectData.projects;
+  }
 };
 </script>
 
---
+++ src/data/projects.js
@@ -0,0 +1,148 @@
+export default  {
+  projects: [
+    {
+      name: 'WiscEnroll Mobile',
+      color1: '#9b0000',
+      color2: '#9b0000',
+      show: false,
+      img_small: false,
+      img: 'mdi-school-outline',
+      no_url: true,
+      url: 'https://www.google.com',
+      subtitle: 'Course Enroll for iOS Devices',
+      description:
+        "I created an iOS front end for UW-Madison's course enrollment app, pulling and updating live user data from the UW system. The source code is private.",
+      skills: [
+        { title: 'Swift', category: 'lang' },
+        { title: 'SwiftUI', category: 'tool' },
+        { title: 'MVC', category: 'dev' },
+        { title: 'URLRequest', category: 'tool' },
+        { title: 'iOS', category: 'platform' },
+        { title: 'macOS', category: 'platform' }
+      ]
+    },
+    {
+      name: 'Hand Gesture Recognizer',
+      color1: '#ff6f00',
+      color2: '#ff6f00',
+      show: false,
+      img_small: false,
+      img: 'mdi-hand-okay',
+      no_url: false,
+      url: 'https://joeholt.github.io/HandGestureRecognizer/',
+      subtitle: 'Interact with you Computer via Hand Gestures',
+      description:
+        "This project contains an application that reads images of a hand from a webcam stream and predicts the gesture of the hand based on the image. Based off of an identified gesture, the application can then modify system behavior by doing things such as changing the volume.",
+      skills: [
+        { title: 'Python', category: 'lang' },
+        { title: 'Computer Vision', category: 'dev' },
+        { title: 'Classification', category: 'dev' },
+        { title: 'Transfer Learning', category: 'dev' },
+        { title: 'Image Segmentation', category: 'dev' },
+        { title: 'OpenCV', category: 'tool' },
+        { title: 'PyTorch', category: 'tool' }
+      ]
+    },
+
+    {
+      name: 'SwiftBoard for iPhone',
+      color1: '#6c5ce7',
+      color2: '#6c5ce7',
+      show: false,
+      img_small: false,
+      img: 'mdi-keyboard',
+      no_url: true,
+      url: 'https://www.google.com',
+      subtitle: 'An Android-esque keyboard for iOS',
+      description:
+        'While in high school, I developed and release SwiftBoard for iPhone as my first app on the App Store. SwiftBoard is an iOS keyboard extension that adds extra functionality on top of the stock keyboard. The source code is private.',
+      skills: [
+        { title: 'Swift', category: 'lang' },
+        { title: 'UIKit', category: 'tool' },
+        { title: '10,000+ units', category: 'acomplisment' },
+        { title: 'Xcode', category: 'tool' },
+        { title: '$5,000+ revenue', category: 'acomplisment' },
+        { title: 'MVC', category: 'dev' },
+        { title: 'iOS', category: 'platform' }
+      ]
+    },
+    {
+      name: 'NEXT Face Demo',
+      color1: '#9b0000',
+      color2: '#9b0000',
+      show: false,
+      img_small: true,
+      img: 'mdi-face-recognition',
+      no_url: false,
+      url: 'https://github.com/stsievert/next-face-demo',
+      subtitle: 'Predict Facial Emotion',
+      description:
+        "Alongside UW-Madison NEXT researchers, I developed a demo that, given an input of the user's face, predicts their emotion. My demo was showcased at the Wisconsin Science Festival.",
+      skills: [
+        { title: 'Python', category: 'lang' },
+        { title: 'Bokeh', category: 'tool' },
+        { title: 'Jupyter', category: 'dev' },
+        { title: 'SciKitLearn', category: 'tool' },
+        { title: 'Web', category: 'platform' },
+        { title: 'AI', category: 'tool' }
+      ]
+    },
+    {
+      name: 'JoeHolt.dev',
+      color1: '#00c853',
+      color2: '#00c853',
+      show: false,
+      img_small: false,
+      img: 'mdi-chart-bubble',
+      url: 'https://github.com/JoeHolt/joeholt.github.io',
+      subtitle: 'The Best Website Out There',
+      description: "I created the website you are currently reading as a way to showcase the work I have done in the field of computer science. The website represents the cumulation of my design skills.",
+      skills: [
+        { title: 'JavaScript', category: 'lang' },
+        { title: 'Vue', category: 'tool' },
+        { title: 'Design', category: 'dev' },
+        { title: 'MVC', category: 'dev' },
+        { title: 'Web', category: 'platform' }
+      ]
+    },
+    {
+      name: 'Spartan',
+      color1: '#3d5afe',
+      color2: '#3d5afe',
+      show: false,
+      img_small: true,
+      img: 'mdi-align-vertical-bottom',
+      url: 'https://github.com/JoeHolt/Spartan-Manufacturing-Web',
+      subtitle: 'Inventory Tracker for my High School',
+      description:
+        'While in high school, I created a multiplatform inventory tracker for my school\'s tech ed department. The tool was designed to track inventories of raw materials, and completed products while being supported on a variety of platforms. The tool is still used today.',
+      skills: [
+        { title: 'Python', category: 'lang' },
+        { title: 'JavaScript', category: 'lang' },
+        { title: 'Swift', category: 'lang' },
+        { title: 'Multiplatform', category: 'platform' },
+        { title: 'Web', category: 'platform' },
+        { title: 'iOS', category: 'platform' },
+        { title: 'macOS', category: 'platform' }
+      ]
+    },
+    {
+      name: 'AHA: AI Assisted Humor',
+      color1: '#ff6f00',
+      color2: '#ff6f00',
+      show: false,
+      img_small: true,
+      img: 'mdi-emoticon-lol-outline',
+      url: 'https://github.com/nextml/AHA',
+      subtitle: 'Preduct Caption Relative Humor',
+      description:
+        'Working along side my fellow researchers, I created a demo that assists users by predicting the relative humor in captions for a New Yorker Caption Contest cartoon.',
+      skills: [
+        { title: 'Python', category: 'lang' },
+        { title: 'JavaScript', category: 'lang' },
+        { title: 'Web', category: 'platform' },
+        { title: 'AI', category: 'tool' }
+      ]
+    }
+  ]
+}
