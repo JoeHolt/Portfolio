b5f2fac46bee90eece4c92a9d9f9b66c2dcbef05
JoeHolt <jpholt2@wisc.edu>
2020-12-17 19:24:34 -0600

    Moved intro data to separate file

--- src/components/IntroPage.vue
+++ src/components/IntroPage.vue
@@ -49,20 +49,20 @@
 </template>
 
 <script>
+import introData from '@/data/intro';
 export default {
   name: 'IntroHeader',
   data() {
     return {
-      preTitle: "HELLO, MY NAME IS...",
-      title: 'JOE HOLT'
+      preTitle: '',
+      title: '',
+      description: ''
     };
   },
-  computed: {
-    description() {
-      var yrs =
-        new Date(new Date() - new Date('11/10/1999')).getFullYear() - 1970;
-      return `Welcome to my portolfio. I am a ${yrs} year old student at the University of Wisconsin-Madison. I enjoy building interesting projects and learning about topics previously unknown. This site highlights some of my favorite works, enjoy!`;
-    }
+  created () {
+    this.title = introData.title;
+    this.preTitle = introData.preTitle;
+    this.description = introData.description;
   }
 };
 </script>
